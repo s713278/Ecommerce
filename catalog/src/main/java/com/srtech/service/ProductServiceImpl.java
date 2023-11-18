973b86208a694887967ed30799007b7937288802
manasanayak <mananayak@gmail.com>
2023-11-17 19:31:32 -0600

    added code

--- catalog/src/main/java/com/srtech/service/ProductServiceImpl.java
+++ catalog/src/main/java/com/srtech/service/ProductServiceImpl.java
@@ -1,7 +1,12 @@
 package com.srtech.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,9 +20,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.srtech.dto.ProductDTO;
+import com.srtech.entity.Category;
 import com.srtech.entity.Image;
 import com.srtech.entity.Product;
+import com.srtech.entity.Sku;
 import com.srtech.exception.ProductNotFoundException;
+import com.srtech.repository.CategoryRepository;
 import com.srtech.repository.ProductRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -33,6 +41,9 @@ public class ProductServiceImpl implements ProductService {
 
 	@Autowired
 	private ProductRepository productRepository;
+
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	@Cacheable(cacheNames = "product", key = "#name")
@@ -93,6 +104,67 @@ public class ProductServiceImpl implements ProductService {
 		productRepository.saveAll(productEntities2);
 		
 		log.debug("saveAll() completed ");
+	}
+
+	@Transactional
+	@Override
+	public void saveAllNew(List<ProductDTO> products) {
+		log.debug("saveAllNew() started");
+		ObjectMapper mapper = new ObjectMapper();
+		/*
+		 * List<Product> productEntities = products.stream() .map(productDTO ->
+		 * mapper.convertValue(productDTO, Product.class)).collect(Collectors.toList());
+		 */
+
+		Map<String, Category> catMap = new HashMap<String, Category>();
+		Set<String> categoryNames = products.stream().map(prod -> prod.getCategory()).collect(Collectors.toSet());
+		for (String cateName : categoryNames) {
+			Category category = new Category();
+			category.setName(cateName);
+			catMap.put(cateName, category);
+		}
+		Iterator<ProductDTO> prodItr = products.iterator();
+		while (prodItr.hasNext()) {
+			ProductDTO prodDto = prodItr.next();
+			if (catMap.containsKey(prodDto.getCategory())) {
+				List<String> images = new ArrayList<String>();
+				images = prodDto.images;
+				List<Image> imagesList = new ArrayList<Image>();
+				for (String str : images) {
+					Image img = Image.builder().imagePath(str).build();
+					imagesList.add(img);
+				}
+
+				List<Sku> skuList = new ArrayList<Sku>();
+				Sku sku = Sku.builder().title(prodDto.getTitle()).description(prodDto.description)
+						.listPrice(prodDto.getPrice()).salePrice(prodDto.getDiscountPercentage())
+						.brand(prodDto.getBrand()).build();
+
+				skuList.add(sku);
+
+				Product prod = Product.builder().title(prodDto.getTitle()).description(prodDto.getDescription())
+						.rating(prodDto.getRating()).thumbnail(prodDto.getThumbnail()).images(imagesList).skus(skuList)
+						.build();
+
+				Category cat = catMap.get(prodDto.getCategory());
+				if (cat.getProducts() == null) {
+					List<Product> newList = new ArrayList<>();
+					newList.add(prod);
+					cat.setProducts(newList);
+				} else {
+					List<Product> existinglist = cat.getProducts();
+					existinglist.add(prod);
+					cat.setProducts(existinglist);
+				}
+				cat.setDescription(prodDto.getCategory());
+			}
+		}
+		List<Category> catsList = new ArrayList<Category>();
+		for (Map.Entry<String, Category> entry : catMap.entrySet()) {
+			catsList.add(entry.getValue());
+		}
+		categoryRepository.saveAll(catsList);
+		log.debug("saveAllNew() completed ");
 	}
 
 	@Override
